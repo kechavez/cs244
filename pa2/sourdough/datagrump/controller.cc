e3d87b6e290644696f1c36e67457facce62f31ba
Jake Smola <smola@stanford.edu>
2017-04-27 15:19:09 -0700

    Part c: intermittent

--- pa2/sourdough/datagrump/controller.cc
+++ pa2/sourdough/datagrump/controller.cc
@@ -10,9 +10,7 @@ Controller::Controller( const bool debug , const unsigned int w )
   : debug_( debug ),
   wsize_(w),
   r_wsize(0),
-  timeout_ack(0),
-  rtt_prev(100), 
-  rtt_delta(0)
+  timeout_ack(0)
 {
   cout << "wSIZE IS : " << wsize_ << endl;
 }
@@ -56,78 +54,33 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
                                /* when the ack was received (by sender) */
 {
   /* Default: take no action */
-  int64_t rtt = timestamp_ack_received - send_timestamp_acked; 
+  int64_t rtt = timestamp_ack_received - send_timestamp_acked;
 	
-  
-/*
-  //if (timestamp_ack_received - timeout_ack > Controller::timeout_ms() && wsize_ >=2 && rtt > Controller::timeout_ms() ) { // Part B
-  int64_t c_thresh = 110; // Part C
-  if (rtt > c_thresh && wsize_ >= 4) { // Part C
+  // cout << "recv is " << timestamp_ack_received << endl;
+  // cout << "send is " << send_timestamp_acked << endl;
+  // cout << "rtt is " << rtt << endl;
+  if (timestamp_ack_received - timeout_ack > Controller::timeout_ms() 
+      && wsize_ >=2 && rtt > Controller::timeout_ms()) {
     timeout_ack = timestamp_ack_received;
-    //wsize_ /= 2; // Part B
-    // if(wsize_>20) {
-    // 	wsize_ -= wsize_/4; // Part C
-    // }
-    // else if (wsize_>5) {
-    //     wsize_-=2;
-    // }
-    if (rtt > c_thresh*2)
-      wsize_ -= wsize_/2;
-    else
-      wsize_ -= wsize_/4;
+    wsize_ /= 2;
     r_wsize = 0;
   }
-  else if (rtt < c_thresh && wsize_< 64) {
-    // r_wsize_++; // Part B
-
-    // Part C
-    if (rtt < c_thresh/2)
-      r_wsize = wsize_;
-    else
-      r_wsize += 2;
-      
-    if ((int)wsize_ - (int)r_wsize <= 0) {
+  else if (timestamp_ack_received - timeout_ack > Controller::timeout_ms() && rtt + 10 > Controller::timeout_ms()) {
+    timeout_ack = timestamp_ack_received;
+    r_wsize = 0;
+    wsize_=(3*wsize_)/4;
+    if (wsize_<1) {
+	wsize_=1;
+    }
+}
+  else {
+    r_wsize++;
+    if (wsize_ - r_wsize == 0) {
       wsize_ += 1;
       r_wsize = 0;
     }
     // cout << "controller wsize = " << wsize_ << endl;
   }
-*/
-
-  ////////////// Start Part D ////////////////////
-  int64_t r_thresh = 105;
-  int64_t d_thresh = 25;
-  rtt_delta = rtt-rtt_prev;
-//  if ( rtt_delta < -2*rtt_delta) 
-  if ( wsize_ > 2 && rtt > r_thresh) {
-    // Decrease window size multiplicatively since we're anticipating more
-    // latency.
-    if ( wsize_ > 4 && rtt_delta > d_thresh*3 ) { 
-      wsize_ -= wsize_/4; // might want to reduce as a function of rtt_delta.
-      // cout << "SHARP Drop at: " << timestamp_ack_received << endl;
-    }
-    // Decrease window size additively since we're experiencing minor latency.
-    else
-      wsize_ -= 1;
-
-    r_wsize = 0;
-  }
-  else if ( wsize_ < 48 && rtt < r_thresh) {
-    r_wsize += 2;
-    // Increase window size multiplicatively since we're anticipating little
-    // latency.
-    if ( rtt_delta < -d_thresh ) {
-      wsize_ += 4;
-      r_wsize = 0;
-    }
-    // Increase window size additively since we're experiencing slightly less
-    // latency.
-    else if ( (int)wsize_ - (int)r_wsize <=0) {
-      wsize_ += 2;
-      r_wsize = 0;
-    }
-  }
-  ////////////// End Part D ////////////////////
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
@@ -136,23 +89,12 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 	 << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
 	 << endl;
   }
-  rtt_prev = rtt;
 }
 
 /* How long to wait (in milliseconds) if there are no acks
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
 {
-  // return 100; // Part B
-  
-  //////////// Start Part D ////////////
-  // If we're experiencing big delay, hold off longer to send.
-  if ( rtt_delta > 125) 
-    return 175;
-  else if (rtt_delta < - 100)
-    return 50;
-
-  return 100;
-  //////////// End Part D ////////////
-  
+  return 105; /* timeout of one second */
 }
+
